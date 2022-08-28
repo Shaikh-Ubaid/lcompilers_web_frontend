0653ba194c2c600ab35b48afe9790ac3f4f9c071
Ubaid <shaikhubaid769@gmail.com>
2022-08-29 02:13:52 +0530

    Currently include lpython.wasm and lpython.js in the repo

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -ex
 # curl https://lfortran.github.io/wasm_builds/data.json -o data.json
 #latest_commit=`curl https://lfortran.github.io/wasm_builds/dev/latest_commit`
 # Set a specific commit to use:
-latest_commit="ce519bee8"
-curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.js" -o public/lfortran.js
-curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.wasm" -o public/lfortran.wasm
-curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.data" -o public/lfortran.data
+# latest_commit="ce519bee8"
+# curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.js" -o public/lfortran.js
+# curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.wasm" -o public/lfortran.wasm
+# curl "https://lfortran.github.io/wasm_builds/dev/$latest_commit/lfortran.data" -o public/lfortran.data
 
 npm run build
 npm run export
